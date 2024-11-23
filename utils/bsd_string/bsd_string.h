5115d193c82d798aea280a8f90a94c3f3d81bfa3
Lucian Mocan <62904583+lucianmocan@users.noreply.github.com>
2024-11-23 11:50:17 +0100

    add def_weak macro

--- utils/bsd_string/bsd_string.h
+++ utils/bsd_string/bsd_string.h
@@ -5,6 +5,7 @@
 
 #include <sys/types.h>
 
+#define DEF_WEAK(fn) __attribute__((weak)) fn
 size_t strlcat(char *dst, const char *src, size_t dsize);
 size_t strlcpy(char *dst, const char *src, size_t dsize);
 #endif
